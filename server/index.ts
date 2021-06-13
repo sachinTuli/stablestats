e64951a8d49d86310809fd4bcec65ee610d4b130
Sachin Tuli <sachintuli0707@gmail.com>
2021-06-13 19:46:50 +0530

    Third try

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,13 @@ mongoose.connect(MONGO_URL, {
 });
 
 app.use(router);
-
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(APP_PORT, () => {
     console.log('Hello baby welcome to my world!');
     console.log("app port",process.env.APP_PORT);
